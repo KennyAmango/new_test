de15ad215d3777c33a88bda91dc64864eb2b609c
MSI\user <kennymax22581997@gmail.com>
2023-05-03 14:31:06 +0800

    first commit

---
+++ src/main/java/com/example/new_test/entity/AA.java
@@ -0,0 +1,50 @@
+package com.example.new_test.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "abc")
+public class AA {
+	
+	@Id
+	@Column(name = "id")
+	private String id;
+	
+	@Column(name = "num")
+	private int num;
+	
+	@Column(name = "name")
+	private String name;
+
+	public AA() {
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/resources/abc.sql
@@ -0,0 +1,32 @@
+
+
+CREATE TABLE IF NOT EXISTS `register1` (
+  `account` varchar(20) NOT NULL,
+  `pwd` varchar(45) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `city` varchar(10) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  `active` tinyint NOT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`account`)
+);
+
+CREATE TABLE IF NOT EXISTS `register2` (
+  `account` varchar(20) NOT NULL,
+  `pwd` varchar(45) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `city` varchar(10) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  `active` tinyint NOT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`account`)
+);
+
+CREATE TABLE IF NOT EXISTS `abc` (
+  `id` varchar(45) NOT NULL,
+  `num` int DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
